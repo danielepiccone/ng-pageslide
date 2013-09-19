e737c37779911daf4de6631a8ec9c62f404772a8
mildtaste <mild.taste@gmail.com>
2013-09-19 16:38:20 +0200

    removed css dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,75 +1,80 @@
-// Karma configuration
-// Generated on Mon Aug 05 2013 12:43:06 GMT+0200 (W. Europe Daylight Time)
+module.exports = function (config) {
+    config.set({
+
+        // Karma configuration
+        // Generated on Mon Aug 05 2013 12:43:06 GMT+0200 (W. Europe Daylight Time)
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  // Libs
-  
-  'components/angular/*.js',
-  'components/angular-mocks/*.js',
-  
-  // Source
-  'src/*.js',
-  
-  // Tests
-  'test/*.js'
-];
+        // list of files / patterns to load in the browser
+        files: [
+            JASMINE,
+            JASMINE_ADAPTER,
+            // Libs
+
+            'components/angular/*.js',
+            'components/angular-mocks/*.js',
+
+            // Source
+            'src/*.js',
+
+            // Tests
+            'test/*.js'
+        ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+        // list of files to exclude
+        exclude: [
+
+        ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit'
+        reporters: ['progress'],
 
 
-// web server port
-port = 9876;
+        // web server port
+        port: 9876,
 
 
-// cli runner port
-runnerPort = 9100;
+        // cli runner port
+        runnerPort: 9100,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
+    });
+}
--- src/angular-pageslide-directive.js
+++ src/angular-pageslide-directive.js
@@ -23,63 +23,118 @@ pageslideDirective.directive('pageslide', [
                 var param = {};
                 param.side = attrs.pageslide || 'right';
                 param.speed = attrs.psSpeed || '0.5';
-
-                /* init */
-                var css_class = 'ng-pageslide ps-hidden';
-                css_class += ' ps-' + param.side;
-
-                /* expose for debug */
-                //deb = el;
+                param.size = '300px';
 
                 /* DOM manipulation */
                 var content = document.getElementById(attrs.href.substr(1));
                 var slider = document.createElement('div');
+                //console.log(content);
                 slider.id = "ng-pageslide";
-                slider.className = css_class;
 
+                /* Style */
+                slider.style.transitionDuration = param.speed + 's';
+                slider.style.webkitTransitionDuration = param.speed + 's';
+                slider.style.zIndex = 1000;
+                slider.style.position = 'fixed';
+                slider.style.transitionProperty = 'width, height';
+
+                slider.style.width = 0;
+                slider.style.height = 0;
+                switch (param.side){
+                    case 'right':
+                        slider.style.height = '100%'; 
+                        slider.style.top = '0px';
+                        slider.style.bottom = '0px';
+                        slider.style.right = '0px';
+                        break;
+                    case 'left':
+                        slider.style.height = '100%';   
+                        slider.style.top = '0px';
+                        slider.style.bottom = '0px';
+                        slider.style.left = '0px';
+                        break;
+                    case 'top':
+                        slider.style.width = '100%';   
+                        slider.style.left = '0px';
+                        slider.style.top = '0px';
+                        slider.style.right = '0px';
+                        break;
+                    case 'bottom':
+                        slider.style.width = '100%'; 
+                        slider.style.bottom = '0px';
+                        slider.style.left = '0px';
+                        slider.style.right = '0px';
+                        break;
+                }
+
+
+                /* Append */
                 document.body.appendChild(slider);
                 slider.appendChild(content);
 
                 //console.log('Pageslider Done.');
 
-                /* set CSS from parameters */
-                if (param.speed){
-                    slider.style.transitionDuration = param.speed + 's';
-                    slider.style.webkitTransitionDuration = param.speed + 's';
-                }
-
                 /*
                 * Events
                 * */
-
-                el[0].onclick = function(e){
+                el[0].addEventListener('click',function(e){
+                    //console.log(0);
                     e.preventDefault();
-                    if (/ps-hidden/.exec(slider.className)){
-                        content.style.display = 'none';
-                        slider.className = slider.className.replace(' ps-hidden','');
-                        slider.className += ' ps-shown';
+                    var shown = slider.style.width != 0 && slider.style.width != 0;
+                    if (shown){
+                        //content.style.display = 'none';
+                        //slider.className = slider.className.replace(' ps-hidden','');
+                        //slider.className += ' ps-shown';
                         //console.log('show');
+                        switch (param.side){
+                            case 'right':
+                                slider.style.width = param.size; 
+                                break;
+                            case 'left':
+                                slider.style.width = param.size; 
+                                break;
+                            case 'top':
+                                slider.style.height = param.size; 
+                                break;
+                            case 'bottom':
+                                slider.style.height = param.size; 
+                                break;
+                        }
                         setTimeout(function(){
                             content.style.display = 'block';
                         },(param.speed * 1000));
 
                     }
 
-                };
+                });
 
                 var close_handler = document.getElementById(attrs.href.substr(1) + '-close');
-
                 if (close_handler){
-                    close_handler.onclick = function(e){
+                    close_handler.addEventListener('click', function(e){
+                        console.log('er');
                         e.preventDefault();
-                        if (/ps-shown/.exec(slider.className)){
+                        if (slider.style.width != 0 && slider.style.width != 0){
                             content.style.display = 'none';
-                            slider.className = slider.className.replace(' ps-shown','');
-                            slider.className += ' ps-hidden';
-                            //console.log('hide');
+                            //slider.className = slider.className.replace(' ps-shown','');
+                            //slider.className += ' ps-hidden';
+                            switch (param.side){
+                                case 'right':
+                                    slider.style.width = '0px'; 
+                                    break;
+                                case 'left':
+                                    slider.style.width =  '0px'; 
+                                    break;
+                                case 'top':
+                                    slider.style.height =  '0px'; 
+                                    break;
+                                case 'bottom':
+                                    slider.style.height =  '0px'; 
+                                    break;
+                            }
                         }
-                    };
-                }
+                    });
+                };
+
 
             }
         };
