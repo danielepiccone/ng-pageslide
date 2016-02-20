0a5fd36fe0d8ce4fb953ddaf8098bb8bc724bd92
Sebastian Wallace <sebjwallace@gmail.com>
2016-02-20 18:06:50 +0000

    Give container/body required styles for ps-push
    When ps-push is enabled the container and body will have the required styles for animation.

--- src/angular-pageslide-directive.js
+++ src/angular-pageslide-directive.js
@@ -90,7 +90,7 @@ angular.module('pageslide-directive', [])
                 slider.style.webkitTransitionDuration = param.speed + 's';
                 slider.style.transitionProperty = 'width, height';
 
-                if (param.squeeze) {
+                if (param.squeeze || param.push) {
                     body.style.position = 'absolute';
                     body.style.transitionDuration = param.speed + 's';
                     body.style.webkitTransitionDuration = param.speed + 's';
