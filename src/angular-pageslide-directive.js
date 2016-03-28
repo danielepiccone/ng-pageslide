0fce0662792666a090c31632bc505c5317d3945e
Daniele Piccone <mild.taste@gmail.com>
2016-03-28 20:17:14 +0200

    Fix missing scope var

--- src/angular-pageslide-directive.js
+++ src/angular-pageslide-directive.js
@@ -14,6 +14,7 @@ angular.module('pageslide-directive', [])
                 psSpeed: '@',
                 psClass: '@',
                 psSize: '@',
+                psZindex: '@',
                 psSqueeze: '@',
                 psCloak: '@',
                 psPush: '@',
