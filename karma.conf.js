f31e757e8f4455ee7a656025446174bb28f47aa4
Daniele Piccone <mild.taste@gmail.com>
2016-07-16 11:23:48 +0200

    Using karma PhantomJS, instead of deprecated PhantomJS2

--- karma.conf.js
+++ karma.conf.js
@@ -61,7 +61,7 @@ module.exports = function (config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS2'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 60000,
